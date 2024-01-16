a380af35e58510e8ae3d14beab6e981017f92079
olusiekwin <olusiekwin@gmail.com>
2024-01-16 16:14:55 +0300

    ✅ Complete task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""
-Module Docs
-"""
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
 def top_ten(subreddit):
-    """
-    Function Docs
-    """
-    url = 'https://www.reddit.com'
-    header = {
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
         'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
-    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
-        url, subreddit, 'top', 10),
-        headers=header,
-        allow_redirects=False)
-    if response.status_code == 200:
-        for post in response.json()['data']['children'][0:10]:
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
             print(post['data']['title'])
     else:
         print(None)
